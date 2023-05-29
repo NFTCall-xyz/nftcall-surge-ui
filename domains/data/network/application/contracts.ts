7d8802b1b147514eb501849b356c326897cb4bf5
gordon199404 <gordon199404@outlook.com>
2023-05-29 17:39:57 +0800

    feat(domains): network add surge contracts

--- domains/data/network/application/contracts.ts
+++ domains/data/network/application/contracts.ts
@@ -2,12 +2,15 @@ import { useWallet } from 'domains'
 import { useMemo } from 'react'
 
 import { ERC20Service } from 'lib/protocol/typechain/erc20'
+import { SurgeUIService, VaultService } from 'lib/protocol/typechain/nftcall-surge'
 
 export const useContracts = () => {
   const { provider } = useWallet()
   const contracts = useMemo(() => {
     return {
       erc20Service: new ERC20Service(provider),
+      surgeUIService: new SurgeUIService(provider),
+      vaultService: new VaultService(provider),
     }
   }, [provider])
 
