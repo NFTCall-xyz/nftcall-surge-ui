17d505401498b85c6a7381909a000eb05722d901
CryptoLory <cryptolory66@gmail.com>
2023-06-05 11:34:19 +1000

    fix: max profit calculation

--- UI/app/trade/OpenOptions/DisplayMaxProfit.tsx
+++ UI/app/trade/OpenOptions/DisplayMaxProfit.tsx
@@ -4,7 +4,6 @@ import { Span } from 'components/Typography'
 import FlexBetween from 'components/flexbox/FlexBetween'
 
 import NumberDisplay from 'lib/math/components/NumberDisplay'
-import { OptionType } from 'lib/protocol/typechain/nftcall-surge'
 import CircularProgress from '@mui/material/CircularProgress'
 import Stack from '@mui/material/Stack'
 import TokenIcon from 'lib/protocol/components/TokenIcon'
@@ -12,20 +11,17 @@ import TokenIcon from 'lib/protocol/components/TokenIcon'
 import { usePageTradeOpenOptions } from '.'
 
 const DisplayMaxProfit: FC = () => {
-  const { optionType, price, premium, tOpenCallOptions } = usePageTradeOpenOptions()
+  const { amount, strikePrice, premium, tOpenCallOptions } = usePageTradeOpenOptions()
   const value = useMemo(() => {
-    if (optionType === OptionType.LONG_CALL) {
-      return 'Limitless'
-    }
     if (premium.loading) return <CircularProgress size={14}/>
-    return <NumberDisplay value={price ? price.minus(premium.value) : 0} />
-  }, [optionType, premium.loading, premium.value, price])
+    return <NumberDisplay value={strikePrice ? strikePrice.value * amount - premium.value : 0} />
+  }, [amount, premium.loading, premium.value, strikePrice])
 
   return (
     <FlexBetween>
       <Span color='text.secondary'>{tOpenCallOptions('maxProfit')}</Span>
       <Stack spacing={0.5} direction="row" alignItems="center" fontSize={14}>
-        {value !== 'Limitless' && <TokenIcon symbol="ETH" sx={{ width: 14, height: 14 }} />}
+        <TokenIcon symbol="ETH" sx={{ width: 14, height: 14 }} />
         {value}
       </Stack>
     </FlexBetween>
