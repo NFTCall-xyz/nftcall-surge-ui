bc69742ebc866a07863eb477cbcbde9c415c90c5
gordon199404 <gordon199404@outlook.com>
2023-07-11 11:10:06 +0800

    feat(dev): refactor useWhyDidYouUpdate

--- app/utils/dev/hooks/useWhyDidYouUpdate.ts
+++ app/utils/dev/hooks/useWhyDidYouUpdate.ts
@@ -1,31 +1,71 @@
-import { noop } from 'lodash'
+import { isEmpty, noop } from 'lodash'
 import { useEffect, useRef } from 'react'
 
 import { log } from '..'
 
 export type IProps = Record<string, any>
 
+function diffArrays(prevProps: any[] = [], props: any[] = []): any {
+  const changedProps: IProps = {}
+  const maxLength = Math.max(prevProps.length, props.length)
+
+  for (let index = 0; index < maxLength; index++) {
+    if (Object.is(prevProps[index], props[index])) continue
+
+    const returnValue = diffObjects(prevProps[index], props[index])
+    if (isEmpty(returnValue)) continue
+    changedProps[index] = returnValue
+  }
+  return changedProps
+}
+
+function diffObjects(prevProps: IProps = {}, props: IProps = {}): any {
+  const allKeys = Object.keys({ ...prevProps, ...props })
+  const changedProps: IProps = {}
+
+  for (const key of allKeys) {
+    const prevValue = prevProps[key]
+    const propValue = props[key]
+
+    if (Object.is(prevValue, propValue) || prevValue === propValue) continue
+
+    const isPrevObject = typeof prevValue == 'object'
+    const isPropObject = typeof propValue == 'object'
+
+    let returnValue
+
+    if (isPrevObject && prevValue instanceof Array) {
+      returnValue = diffArrays(prevValue, propValue)
+    } else if (isPrevObject && prevValue._isBigNumber) {
+      if (prevValue.toString && prevValue.toString() === propValue.toString()) continue
+      returnValue = { from: prevValue, to: propValue }
+    } else if (isPrevObject && isPropObject) {
+      returnValue = diffObjects(prevValue, propValue)
+    } else {
+      returnValue = { from: prevValue, to: propValue }
+    }
+
+    if (!isEmpty(returnValue)) {
+      changedProps[key] = returnValue
+    }
+  }
+
+  return changedProps
+}
+
 export const useWhyDidYouUpdate = __DEV__
-  ? (componentName: string, props: IProps) => {
+  ? (componentName: string, props: any) => {
       const prevProps = useRef<IProps>({})
 
       useEffect(() => {
         if (prevProps.current && !props) {
           log(componentName, 'removed')
         } else if (prevProps.current) {
-          const allKeys = Object.keys({ ...prevProps.current, ...props })
-          const changedProps: IProps = {}
-
-          allKeys.forEach((key) => {
-            if (!Object.is(prevProps.current[key], props[key])) {
-              changedProps[key] = {
-                from: prevProps.current[key],
-                to: props[key],
-              }
-            }
-          })
-
-          if (Object.keys(changedProps).length) {
+          const changedProps: IProps =
+            prevProps.current instanceof Array
+              ? diffArrays(prevProps.current, props)
+              : diffObjects(prevProps.current, props)
+          if (!isEmpty(changedProps)) {
             log(componentName, changedProps)
           }
         }
