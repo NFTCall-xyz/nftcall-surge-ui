a651bef9914925f8d5fe259eba72824b44f7b66a
gordon199404 <gordon199404@outlook.com>
2023-07-18 10:01:46 +0800

    feat(graphql): add vault

---
+++ lib/graphql/vault/index.ts
@@ -0,0 +1,20 @@
+import { request as RQ } from 'graphql-request'
+
+import document from './active/index.graphql'
+
+export type GraphqlVaultBaseData = {
+  totalTradingVolume: string
+  totalPremiumCollected: string
+  totalTrades: number
+}
+
+export type ResponseType = { vaults: GraphqlVaultBaseData[] }
+
+export type RequestType = {
+  id?: string
+}
+
+export const graphqlRequest = (url: string, variables: RequestType) => {
+  variables.id = variables.id || '1'
+  return RQ<ResponseType>(url, document, variables)
+}
