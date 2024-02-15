5605b4f7bb6da46f4c110e552fe4fa1deae9c065
gordon199404 <gordon199404@outlook.com>
2024-02-15 12:30:01 +0800

    yarn codegen

---
+++ lib/openapi/models/CreateBlockchainPriceDto.ts
@@ -0,0 +1,35 @@
+/* generated using openapi-typescript-codegen -- do no edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+
+export type CreateBlockchainPriceDto = {
+  /**
+   * 波动率
+   */
+  volatility: string
+  /**
+   * 上链状态
+   */
+  status: string
+  /**
+   * 更新原因
+   */
+  updateReason: string
+  /**
+   * 集合名称
+   */
+  collectionName: string
+  /**
+   * 地板价
+   */
+  floorPrice: string
+  /**
+   * 操作时间
+   */
+  operationTime: string
+  /**
+   * 实体的备注信息
+   */
+  remark?: string
+}
--- lib/openapi/services/BlockchainPriceService.ts
+++ lib/openapi/services/BlockchainPriceService.ts
@@ -8,10 +8,35 @@
 import type { CancelablePromise } from '../core/CancelablePromise'
 import { OpenAPI } from '../core/OpenAPI'
 import { request as __request } from '../core/request'
+import type { AdminBlockchainPriceDto } from '../models/AdminBlockchainPriceDto'
 import type { AdminBlockchainPriceFindAllRequest } from '../models/AdminBlockchainPriceFindAllRequest'
 import type { AdminBlockchainPriceFindAllResponse } from '../models/AdminBlockchainPriceFindAllResponse'
+import type { CreateBlockchainPriceDto } from '../models/CreateBlockchainPriceDto'
+import type { UpdateAdminBlockchainPriceDto } from '../models/UpdateAdminBlockchainPriceDto'
 
 export class BlockchainPriceService {
+  /**
+   * 创建一个新的 链上价格表(BlockchainPrice)
+   * @param requestBody
+   * @param xToken token for authentication
+   * @returns AdminBlockchainPriceDto 链上价格表(BlockchainPrice) 已创建
+   * @throws ApiError
+   */
+  public static create(
+    requestBody: CreateBlockchainPriceDto,
+    xToken?: string
+  ): CancelablePromise<AdminBlockchainPriceDto> {
+    return __request(OpenAPI, {
+      method: 'POST',
+      url: '/api/tasks/blockchain-price',
+      headers: {
+        'X-Token': xToken,
+      },
+      body: requestBody,
+      mediaType: 'application/json',
+    })
+  }
+
   /**
    * 获取 链上价格表(BlockchainPrice) 列表
    * @param requestBody
@@ -26,6 +51,57 @@ export class BlockchainPriceService {
       url: '/api/tasks/blockchain-price/findAll',
       body: requestBody,
       mediaType: 'application/json',
+    })
+  }
+
+  /**
+   * 更新一个 链上价格表(BlockchainPrice)
+   * @param id 链上价格表(BlockchainPrice) ID
+   * @param requestBody
+   * @param xToken token for authentication
+   * @returns AdminBlockchainPriceDto 链上价格表(BlockchainPrice) 已更新
+   * @throws ApiError
+   */
+  public static update(
+    id: number,
+    requestBody: UpdateAdminBlockchainPriceDto,
+    xToken?: string
+  ): CancelablePromise<AdminBlockchainPriceDto> {
+    return __request(OpenAPI, {
+      method: 'PUT',
+      url: '/api/tasks/blockchain-price/{id}',
+      path: {
+        id: id,
+      },
+      headers: {
+        'X-Token': xToken,
+      },
+      body: requestBody,
+      mediaType: 'application/json',
+    })
+  }
+
+  /**
+   * 删除一个 链上价格表(BlockchainPrice)
+   * @param id 链上价格表(BlockchainPrice) ID
+   * @param isSoftDelete 是否软删除
+   * @param xToken token for authentication
+   * @returns any 链上价格表(BlockchainPrice) 已删除
+   * @throws ApiError
+   */
+  public static remove(id: number, isSoftDelete?: boolean, xToken?: string): CancelablePromise<any> {
+    return __request(OpenAPI, {
+      method: 'DELETE',
+      url: '/api/tasks/blockchain-price/{id}',
+      path: {
+        id: id,
+      },
+      headers: {
+        'X-Token': xToken,
+      },
+      query: {
+        isSoftDelete: isSoftDelete,
+      },
     })
   }
 }
